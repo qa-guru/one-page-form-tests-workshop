b3f7dfb8aed556ee9ba47f6f7a9b52a26628db66
svasenkov <aanher@gmail.com>
2026-07-06 21:08:48 +0200

    improved LoginTests and added LogoutTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -9,11 +10,14 @@ public class LoginTests extends TestBase {
     @Test
     public void successfulAuthorization() {
         open("/login.html");
+        $("[data-testid=login-form-title]").shouldHave(text("Login Form"));
 
         $("[data-testid=login-input]").setValue("user1");
         $("[data-testid=password-input]").setValue("password1");
         $("[data-testid=submit-button]").click();
 
+        $("[data-testid=login-form]").shouldNotBe(visible);
+        $("[data-testid=logged-in-title]").shouldHave(text("Logged In Form"));
         $("[data-testid=welcome-message]").shouldHave(text("Welcome, user1!"));
     }
 
---
+++ src/test/java/LogoutTests.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class LogoutTests extends TestBase {
+
+    @Test
+    @Disabled("Bad practice - long e2e test, split to atomic tests")
+    public void successfulLogout() {
+        open("/login.html");
+        $("[data-testid=login-input]").setValue("user1");
+        $("[data-testid=password-input]").setValue("password1");
+        $("[data-testid=submit-button]").click();
+        $("[data-testid=welcome-message]").shouldHave(text("Welcome, user1!"));
+
+        $("[data-testid=logout-button]").click();
+
+        $("[data-testid=success-panel]").shouldNotBe(visible);
+        $("[data-testid=login-form-title]").shouldHave(text("Login Form"));
+    }
+
+    @Test
+    public void successfulLogoutWithLocalstorageAuth() {
+        open("/login.html");
+        localStorage().setItem("authUser", "user1");
+        open("/login.html");
+
+        $("[data-testid=logout-button]").click();
+
+        $("[data-testid=success-panel]").shouldNotBe(visible);
+        $("[data-testid=login-form-title]").shouldHave(text("Login Form"));
+    }
+}
